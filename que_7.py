c21a043822d1dc0a007d71d696779763df632397
samartawale <tawalesamar@gmail.com>
2025-05-23 11:36:03 +0530

    Create que_7.py

---
+++ que_7.py
@@ -0,0 +1,17 @@
+#This code take percentage from user and give a grade as per percentage.
+
+perccentage=float(input("Enter Percentage: "))
+if percentage<40:
+  print("Grade F")
+elif 40<=percentage<50:
+  print("Grade E")
+elif 50<=percentage<60:
+  print("Grade D")
+elif 60<=percentage<70:
+  print("Grade C")
+elif 70<=percentage<80:
+  print("Grade B")
+elif 80<percentage<90:
+  print("Grade A")
+elif 90<percentage<100:
+  print("Grade A+")
